ca488c935d95331094929b03eaaa18e37cad7323
Catarina Costa <catarinacosta217@gmail.com>
2017-04-15 12:08:49 +0100

    Create Button component

---
+++ react/src/components/Button.jsx
@@ -0,0 +1,17 @@
+import React, { PropTypes } from 'react';
+
+/**
+ * Render function
+ *
+ * @params props Component properties
+ */
+function Button(props) {
+  return (
+    <div className="form-group">
+        <button className="submit-button" onClick={props.onSubmit}>{props.text}</button>
+    </div>
+  );
+}
+
+
+export default Button;
